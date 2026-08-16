40b8f10205544305ab79072f231127cb98651a6a
vishwa-62 <vishwaofficial62@gmail.com>
2026-08-16 12:09:05 +0530

    Set base: ./ in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/Smart-CLassroom-Management-software-for-Enhanced-Learning-Environments/' : './',
+  base: './',
   server: {
     port: 5180,
     strictPort: false,
